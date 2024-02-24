a8875b92dbc7ded422e7c74545c139a57da90a5f
Justinecirera <128766246+Justinecirera@users.noreply.github.com>
2024-02-24 15:30:06 +0800

    Main files and Folders

---
+++ InventorySystem/resources/views/welcome.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- tailwind link cdn -->
+  <script src="https://cdn.tailwindcss.com"></script>
+</head> 
+<body>
+    <!-- header -->
+    <header class="bg-red-900 text-white p-4">
+    <div class="container mx-auto flex flex-wrap justify-between items-center">
+        <h1 class="text-2xl md:text-4xl font-bold flex-1">HMP Co. Inventory Management System</h1>
+        <nav class="w-full md:w-auto">
+            <ul class="flex flex-col md:flex-row md:space-x-4">
+                <li><a href="/about" class="text-white">About Us</a></li>
+                <li><a href="/pricing" class="text-white">Pricing</a></li>
+                <li><a href="/contact" class="text-white">Contact Us</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
+    <!-- main content -->
+</body>
+</html>
